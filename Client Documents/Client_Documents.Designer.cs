148739ae29e015c8558418945cb1bb430d2dc957
one1erp <avigaile@one1.co.il>
2024-04-09 09:02:28 +0300

    Initial commit with additional files

---
+++ Client Documents/Client_Documents.Designer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Client_Documents
+{
+    partial class Client_Documents
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.lblError = new System.Windows.Forms.Label();
+            this.radPanel1 = new Telerik.WinControls.UI.RadPanel();
+            this.lblHeader = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).BeginInit();
+            this.radPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(348, 277);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(378, 72);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(744, 450);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.Location = new System.Drawing.Point(786, 46);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(98, 13);
+            this.lblError.TabIndex = 5;
+            this.lblError.Text = "לא קיים סקר חוזה";
+            this.lblError.Visible = false;
+            // 
+            // radPanel1
+            // 
+            this.radPanel1.Controls.Add(this.lblHeader);
+            this.radPanel1.Controls.Add(this.listBox1);
+            this.radPanel1.Controls.Add(this.lblError);
+            this.radPanel1.Controls.Add(this.webBrowser1);
+            this.radPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radPanel1.Location = new System.Drawing.Point(0, 0);
+            this.radPanel1.Name = "radPanel1";
+            this.radPanel1.Size = new System.Drawing.Size(1165, 554);
+            this.radPanel1.TabIndex = 6;
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Font = new System.Drawing.Font("David", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblHeader.ForeColor = System.Drawing.Color.Blue;
+            this.lblHeader.Location = new System.Drawing.Point(437, 21);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblHeader.Size = new System.Drawing.Size(151, 21);
+            this.lblHeader.TabIndex = 34;
+            this.lblHeader.Text = "מסמכי סקר חוזה";
+            // 
+            // Client_Documents
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.radPanel1);
+            this.Name = "Client_Documents";
+            this.Size = new System.Drawing.Size(1165, 554);
+            this.Resize += new System.EventHandler(this.Client_Documents_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanel1)).EndInit();
+            this.radPanel1.ResumeLayout(false);
+            this.radPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label lblError;
+        private Telerik.WinControls.UI.RadPanel radPanel1;
+        private System.Windows.Forms.Label lblHeader;
+
+    }
+}
---
+++ Client Documents/Client_Documents.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using Common;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using DAL;
+using LSExtensionControlLib;
+using LSSERVICEPROVIDERLib;
+
+namespace Client_Documents
+{
+    [ComVisible(true)]
+    [ProgId("NautilusExtensions.Client_Documents")]
+    public partial class Client_Documents : UserControl, IExtensionControl
+    {
+        public Client_Documents()
+        {
+            InitializeComponent();
+            BackColor = Color.FromName("Control");
+        }
+        string _path = null;
+        #region Private members
+
+        /// <summary>
+        ///     Process XML service provider string
+        /// </summary>
+        private const string PROCESS_XML_SP = "ProcessXML";
+
+        /// <summary>
+        ///     Table name
+        /// </summary>
+        private const string TABLE_NAME = "TABLE";
+
+        /// <summary>
+        ///     ID Field Name
+        /// </summary>
+        private const string FIELD_NAME_ID = "TABLE_ID";
+
+        private Client client;
+        private IDataLayer dal;
+
+        /// <summary>
+        ///     XML Processor object
+        /// </summary>
+        private INautilusProcessXML nautilusProcessXML;
+
+        private INautilusDBConnection ntlCon;
+
+        /// <summary>
+        ///     Id of the record selected
+        /// </summary>
+        private double recordID;
+
+        /// <summary>
+        ///     Service Provider object
+        /// </summary>
+        private INautilusServiceProvider serviceProvider;
+
+        private IExtensionControlSite site;
+
+        #endregion
+
+        #region IExtensionControl
+
+        public void EnterPage()
+        {
+            Utils.CreateConstring(ntlCon);
+            lblError.Visible = false;
+            GetClientById(recordID);
+        }
+
+
+        public void ExitPage()
+        {
+            dal.Close();
+            // throw new NotImplementedException();
+        }
+
+        public void Internationalise()
+        {
+            //   throw new NotImplementedException();
+        }
+
+        public void PreDisplay()
+        {
+            //   throw new NotImplementedException();
+        }
+
+        public void RestoreSettings(int hKey)
+        {
+            //  throw new NotImplementedException();
+        }
+
+        public void SaveData()
+        {
+            //  throw new NotImplementedException();
+        }
+
+        public void SaveSettings(int hKey)
+        {
+            //  throw new NotImplementedException();
+        }
+
+        public void SetReadOnly(bool readOnly)
+        {
+            //throw new NotImplementedException();
+        }
+
+
+        public void SetServiceProvider(object sp)
+        {
+            // Check the service provider object is not null
+            if (sp != null)
+            {
+                // Cast the object to the correct type
+                serviceProvider = sp as INautilusServiceProvider;
+
+                // Using the service provider object get the XML Processor interface
+                // We will use this object to get information from the database
+                nautilusProcessXML = Utils.GetXmlProcessor(serviceProvider);
+
+
+                ntlCon = Utils.GetNtlsCon(serviceProvider);
+            }
+        }
+
+        /// <summary>
+        ///     Called once at the start of the extension page’s life.
+        ///     It is used to pass in a reference to a site object that the page can use to call back into Nautilus.
+        /// </summary>
+        /// <param name="cs">Site object</param>
+        public void SetSite(object cs)
+        {
+            // Check site object is not null
+            if (cs != null)
+            {
+                // Cast the object to the correct type
+                site = cs as IExtensionControlSite;
+            }
+        }
+
+        /// <summary>
+        ///     Called once at the start of an Extension page’s life to tell the page to load information from the record.
+        ///     The Extension page should use the site object to read the required information.
+        ///     It should make sure to read all the information it will require during the SetupData method as calls to the site
+        ///     object to read information are only allowed during the SetupData method.
+        /// </summary>
+        public void SetupData()
+        {
+            bool flag;
+            // Check site object is not null
+            if (site != null)
+            {
+                // Set the page name
+                site.SetPageName("Documents");
+
+                // Get the record id
+                site.GetDoubleValue("CLIENT_ID", out recordID, out flag);
+            }
+        }
+
+        #endregion
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+
+            string path = _path;// client.LocationFile + client.ContractFileName + "\\";
+            object wordName = listBox1.SelectedItem;
+            string uri = Path.Combine(path, wordName.ToString());
+            webBrowser1.Navigate(new Uri(uri));
+
+
+        }
+
+        private void GetClientById(double clientId)
+        {
+            listBox1.Items.Clear();
+            dal = new DataLayer();
+            dal.Connect();
+            client = dal.GetClientByID(clientId);
+
+            //get path
+            //     _path = dal.GetPathOfContractFiles(client.ClientId);
+
+            var ph = dal.GetPhraseByName("Location folders");
+            var pe = ph.PhraseEntries.Where(p => p.PhraseDescription == "Client documents").FirstOrDefault();
+            var clientDocumentsPath = pe.PhraseName;
+
+
+
+            if (client.ContractFileName != null)
+            {
+                _path = Path.Combine(clientDocumentsPath, client.ContractFileName);
+                try
+                {
+                    //get directory
+                    var dirInfo = new DirectoryInfo(_path);
+
+                    //get word Documents from  directory
+                    FileInfo[] wordFiles = dirInfo.GetFiles("*.pdf");
+                    //get files names for this client
+                    string[] fileNames = wordFiles.Where(x => x.Name.Contains(client.Name)).Select(x => x.Name).ToArray();
+
+                    listBox1.Items.AddRange(fileNames);
+                }
+                catch (Exception ex)
+                {
+
+                    lblError.Visible = true;
+                    lblError.Text = "אין אפשרות למצוא את התיקייה המבוקשת.";
+                    Logger.WriteLogFile(ex);
+
+                }
+            }
+            else
+            {
+                lblError.Visible = true;
+            }
+        }
+
+
+
+        private PhraseEntry contractPath;
+        private PhraseEntry contractFolder;
+        private void GetFolderLocations()
+        {
+            try
+            {
+                var phraseH = dal.GetPhraseByName("Location folders");
+                contractPath = phraseH.PhraseEntries.FirstOrDefault(x => x.PhraseDescription == "Client documents");
+                contractFolder = phraseH.PhraseEntries.FirstOrDefault(x => x.PhraseDescription == "Contract documents");
+            }
+            catch (Exception ex)
+            {
+                lblError.Visible = true;
+                lblError.Text = "שגיאה ביצירת כתובת המסמך";
+            }
+        }
+        private void Client_Documents_Resize(object sender, EventArgs e)
+        {
+            lblHeader.Location = new Point(Width / 2 - lblHeader.Width / 2, lblHeader.Location.Y);
+        }
+
+
+    }
+}
